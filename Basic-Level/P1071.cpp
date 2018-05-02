27fe2270f8a22b10e726d309a61d9aacd1e09b4a
呉真 <kuretru@gmail.com>
2018-05-02 21:31:00 +0800

    Added Basic-Level P1071

---
+++ Basic-Level/P1071.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main()
+{
+    int T, K;
+    std::cin >> T >> K;
+    while (K--)
+    {
+        int n1, b, t, n2;
+        std::cin >> n1 >> b >> t >> n2;
+        if (t > T)
+        {
+            printf("Not enough tokens.  Total = %d.\n", T);
+            continue;
+        }
+        if ((b == 0 && n1 > n2) || (b == 1 && n1 < n2))
+            printf("Win %d!  Total = %d.\n", t, T += t);
+        else
+            printf("Lose %d.  Total = %d.\n", t, (T -= t) < 0 ? 0 : T);
+        if (T <= 0)
+        {
+            std::cout << "Game Over." << std::endl;
+            break;
+        }
+    }
+    return 0;
+}
