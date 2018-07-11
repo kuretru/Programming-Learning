9065418f322840f4c93d25435b4d5a9b07e4ccf2
呉真 <kuretru@gmail.com>
2018-07-11 18:47:38 +0800

    Added Advanced-Level P1004

---
+++ Advanced-Level/P1004.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::vector<int> data[100];
+    int N, M;
+    std::cin >> N >> M;
+    while (M-- > 0)
+    {
+        int id, K, child;
+        std::cin >> id >> K;
+        while (K--)
+        {
+            std::cin >> child;
+            data[id].push_back(child);
+        }
+    }
+    std::queue<int> q;
+    q.push(1);
+    while (!q.empty())
+    {
+        int back = q.back(), count = 0;
+        while (true)
+        {
+            int id = q.front();
+            q.pop();
+            if(data[id].empty())
+                count++;
+            for (auto iter = data[id].begin(); iter != data[id].end(); iter++)
+                q.push(*iter);
+            if (id == back)
+                break;
+        }
+        if(q.empty())
+            std::cout << count << std::endl;
+        else
+            std::cout << count << " ";
+    }
+    return 0;
+}
