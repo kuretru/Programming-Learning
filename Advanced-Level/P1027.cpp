c0ae54b0bc03d41cd9b9644e2668fe5baeff84a5
呉真 <kuretru@gmail.com>
2018-07-11 22:37:21 +0800

    Added Advanced-Level P1027

---
+++ Advanced-Level/P1027.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+const char keys[] = "0123456789ABC";
+int main()
+{
+    std::cout << "#";
+    int color;
+    for (int i = 0; i < 3; i++)
+    {
+        std::cin >> color;
+        printf("%c%c", keys[color / 13], keys[color % 13]);
+    }
+    std::cout << std::endl;
+    return 0;
+}
