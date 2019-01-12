f9fa2622bff97e67196bdb365806cf21cfbf8756
呉真 <kuretru@gmail.com>
2019-01-12 16:18:30 +0800

    新增：甲级-P1152 Google Recruitment

---
+++ Advanced-Level/P1152.Google_Recruitment.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+bool isPrime(int number)
+{
+    if (number < 2)
+        return false;
+    if (number != 2 && number % 2 == 0)
+        return false;
+    if (number != 3 && number % 3 == 0)
+        return false;
+    if (number != 5 && number % 5 == 0)
+        return false;
+    if (number != 7 && number % 7 == 0)
+        return false;
+    int length = std::sqrt(number);
+    for (int i = 11; i < length; i++)
+        if (number % i == 0)
+            return false;
+    return true;
+}
+int main()
+{
+    int L, K;
+    std::string key;
+    std::cin >> L >> K >> key;
+    for (int i = 0; i <= L - K; i++)
+    {
+        std::string sub = key.substr(i, K);
+        if (isPrime(std::stoi(sub)))
+        {
+            std::cout << sub << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "404" << std::endl;
+    return 0;
+}
