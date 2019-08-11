97c85b5a619527911dc1be4183222e45eed76580
呉真 <kuretru@gmail.com>
2019-08-11 17:26:54 +0800

    新增：中国大学MOOC-数据结构.第九周作业 1 堆排序、归并排序、基数排序实现

--- PAT/Data-Structure/09-排序1 排序.cpp
+++ PAT/Data-Structure/09-排序1 排序.cpp
@@ -11,9 +11,9 @@ void shell_sort(vector<int> &data);     // 希尔排序 35ms 554ms 6852ms 81538m
 void bubble_sort(vector<int> &data);    // 冒泡排序 TLE*3
 void quick_sort(vector<int> &data);     // 快速排序 59ms 303ms 3579ms 40963ms
 void selection_sort(vector<int> &data); // 选择排序 TLE*5
-void heap_sort(vector<int> &data);      // 堆排序
-void merge_sort(vector<int> &data);     // 归并排序
-void radix_sort(vector<int> &data);     // 基数排序
+void heap_sort(vector<int> &data);      // 堆排序　 46ms 473ms 7103ms 98014ms
+void merge_sort(vector<int> &data);     // 归并排序 33ms 491ms 5696ms 64947ms
+void radix_sort(vector<int> &data);     // 基数排序 38ms 780ms 8007ms 79211ms
 
 int quick_sort_pivot_median3(vector<int> &data, int left, int right); // 快速排序枢轴三分取中
 int quick_sort_pivot_random(vector<int> &data, int left, int right);  // 快速排序枢轴取随机数字
@@ -23,10 +23,10 @@ typedef void (*sort_interface)(vector<int> &data);                              
 typedef int (*quick_sort_pivot_interface)(vector<int> &data, int left, int right); // 快速排序枢轴函数接口
 
 // 需更改的常量
-sort_interface sort = quick_sort;                                       // 指定排序函数实现
+sort_interface sort = radix_sort;                                       // 指定排序函数实现
 quick_sort_pivot_interface quick_sort_pivot = quick_sort_pivot_median3; // 指定快速排序枢轴函数实现，将枢轴移至第一个位置
-int QUICK_SORT_THRESHOLD = -1;                                          // 快速排序转插入排序的阈值，-1为仅使用快速排序
-bool DEBUG = true;                                                      // 调试开关
+int QUICK_SORT_THRESHOLD = 50;                                          // 快速排序转插入排序的阈值，-1为仅使用快速排序
+bool DEBUG = false;                                                     // 调试开关
 
 int main()
 {
@@ -96,15 +96,15 @@ void insertion_sort(vector<int> &data)
 
 void shell_sort(vector<int> &data)
 {
-    size_t sequence;
+    int sequence = 0;
     int sedgewick[] = {67084289, 37730305, 16764929,
                        9427969, 4188161, 2354689, 1045505, 587521, 260609, 146305,
                        64769, 36289, 16001, 8929, 3905, 2161, 929, 505, 209, 109, 41, 19, 5, 1, 0};
-    for (sequence = 0; sedgewick[sequence] >= data.size(); sequence++)
+    for (sequence = 0; sedgewick[sequence] >= (int)data.size(); sequence++)
         ;
     for (int d = sedgewick[sequence]; d > 0; d = sedgewick[++sequence])
     {
-        for (size_t i = d, j; i < data.size(); i++)
+        for (int i = d, j; i < (int)data.size(); i++)
         {
             int tmp = data[i];
             for (j = i; j >= d && data[j - d] > tmp; j -= d)
@@ -191,5 +191,92 @@ void selection_sort(vector<int> &data)
         auto min = std::min_element(iter, data.end());
         if (iter != min)
             std::swap(*iter, *min);
+    }
+}
+
+void heap_sort_adjust(vector<int> &data, int key, int length)
+{
+    int value = data[key], parent, child;
+    for (parent = key, child = (parent << 1) + 1; child < length; child = (parent << 1) + 1)
+    {
+        if ((child + 1) < length && data[child + 1] > data[child])
+            child++;
+        if (value > data[child])
+            break;
+        data[parent] = data[child];
+        parent = child;
+    }
+    data[parent] = value;
+}
+
+void heap_sort(vector<int> &data)
+{
+    for (int i = (data.size() - 1) >> 1; i >= 0; i--)
+        heap_sort_adjust(data, i, data.size());
+    for (int i = data.size() - 1; i > 0; i--)
+    {
+        std::swap(data[0], data[i]);
+        heap_sort_adjust(data, 0, i);
+    }
+}
+
+void merge_sort_merge(vector<int> &data, vector<int> &tmp, int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = i;
+    for (; i <= mid && j <= high; k++)
+    {
+        if (data[i] < data[j])
+            tmp[k] = data[i++];
+        else
+            tmp[k] = data[j++];
+    }
+    while (i <= mid)
+        tmp[k++] = data[i++];
+    while (j <= high)
+        tmp[k++] = data[j++];
+    for (i = low; i <= high; i++)
+        data[i] = tmp[i];
+}
+
+void merge_sort_core(vector<int> &data, vector<int> &tmp, int left, int right)
+{
+    if (left >= right)
+        return;
+    int mid = (left + right) >> 1;
+    merge_sort_core(data, tmp, left, mid);
+    merge_sort_core(data, tmp, mid + 1, right);
+    merge_sort_merge(data, tmp, left, mid, right);
+}
+
+void merge_sort(vector<int> &data)
+{
+    vector<int> tmp(data.size());
+    merge_sort_core(data, tmp, 0, data.size() - 1);
+}
+
+void radix_sort_merge(vector<int> &data, vector<vector<int>> &buckets, int &index, int start, int end)
+{
+    for (int i = start; i < end; i++)
+        for (auto iter = buckets[i].begin(); iter != buckets[i].end(); iter++)
+            data[index++] = *iter;
+}
+
+void radix_sort(vector<int> &data)
+{
+    vector<vector<int>> buckets(16);
+    for (unsigned int times = 0, hex = 0xf; times <= 28; times += 4, hex <<= 4)
+    {
+        for (auto iter = buckets.begin(); iter != buckets.end(); iter++)
+            iter->clear();
+        for (auto iter = data.begin(); iter != data.end(); iter++)
+            buckets[(*iter & hex) >> times].push_back(*iter);
+        int index = 0;
+        if (times == 28)
+        {
+            radix_sort_merge(data, buckets, index, 8, 16);
+            radix_sort_merge(data, buckets, index, 0, 8);
+        }
+        else
+            radix_sort_merge(data, buckets, index, 0, 16);
     }
 }
