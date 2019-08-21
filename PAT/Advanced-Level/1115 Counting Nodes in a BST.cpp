f5c9f1f672413983131034910f9def4ca6fdaed5
呉真 <kuretru@gmail.com>
2019-08-21 18:29:49 +0800

    新增：PAT-甲级.1115 Counting Nodes in a BST

--- PAT/Advanced-Level/1115 Counting Nodes in a BST.cpp
+++ PAT/Advanced-Level/1115 Counting Nodes in a BST.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <map>
-const int empty = 19960203;
-std::map<int, int> result;
+
 struct node
 {
-    node *left = NULL, *right = NULL;
-    int data = empty;
+    int data = INT32_MAX;
+    node *left = NULL;
+    node *right = NULL;
 };
-void insert(node *head, int key, int level)
+
+void insert(node *head, int key, std::map<int, int> &result, int level)
 {
-    if (head->data == empty)
+    if (head->data == INT32_MAX)
     {
         head->data = key;
         result[level]++;
@@ -18,27 +19,29 @@ void insert(node *head, int key, int level)
     {
         if (head->left == NULL)
             head->left = new node();
-        insert(head->left, key, level + 1);
+        insert(head->left, key, result, level + 1);
     }
     else if (key > head->data)
     {
         if (head->right == NULL)
             head->right = new node();
-        insert(head->right, key, level + 1);
+        insert(head->right, key, result, level + 1);
     }
 }
+
 int main()
 {
     int N, key;
     std::cin >> N;
     node *head = new node();
+    std::map<int, int> result;
     while (N--)
     {
         std::cin >> key;
-        insert(head, key, 0);
+        insert(head, key, result, 0);
     }
     int x = result.rbegin()->second;
     int y = result.size() == 1 ? 0 : (++result.rbegin())->second;
-    printf("%d + %d = %d\n", x, y, x + y);
+    std::cout << x << " + " << y << " = " << x + y;
     return 0;
 }
