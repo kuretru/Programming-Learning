f852da9377a13faca562c675aaa73991c88fbdbe
呉真 <kuretru@gmail.com>
2019-08-23 15:27:58 +0800

    新增：PAT-甲级.1117 Eddington Number

---
+++ PAT/Advanced-Level/1117 Eddington Numbe.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N, E = 0;
+    std::cin >> N;
+    std::vector<int> data(N);
+    for (int i = 0; i < N; i++)
+        std::cin >> data[i];
+    std::sort(data.rbegin(), data.rend());
+    while (data[E] > E + 1 && E < N)
+        E++;
+    std::cout << E << std::endl;
+    return 0;
+}
