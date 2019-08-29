3f0a8686a7f462d5efff91d6862c1603c9cb4059
呉真 <kuretru@gmail.com>
2019-08-29 19:21:42 +0800

    新增：LeetCode，占坑

---
+++ LeetCode/1.两数之和.cpp
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
---
+++ LeetCode/1.两数之和.java
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+}
+
