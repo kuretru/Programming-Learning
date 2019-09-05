aa107fd0baf2910e24b3dc2032e0cae0e48c36d5
呉真 <kuretru@gmail.com>
2019-09-05 19:55:26 +0800

    新增：PAT-甲级.1085 Perfect Sequence

---
+++ PAT/Advanced-Level/1085 Perfect Sequence.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    long long N = 0, P = 0;
+    std::cin >> N >> P;
+    std::vector<int> data(N);
+    for (int i = 0; i < N; i++)
+        std::cin >> data[i];
+    std::sort(data.begin(), data.end());
+    int result = 1;
+    for (int i = 0; i + result <= data.size(); i++)
+    {
+        for (int j = i + result; j < data.size(); j++)
+        {
+            if (data[j] > data[i] * P)
+                break;
+            result++;
+        }
+    }
+    std::cout << result << std::endl;
+    return 0;
+}
