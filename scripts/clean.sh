89dcc61e2eb6793efda8146173ff74bcf04aeab5
呉真 <kuretru@gmail.com>
2019-09-12 16:58:55 +0800

    新增：显示清理的文件及目录个数

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-find .. -name *.dSYM -type d -print -exec rm -rf {} \;
-find .. -name *.out -type f -print -exec rm -rf {} \;
+old_dir=$(find .. -name *.dSYM -type d -print | wc -l)
+old_file=$(find .. -name *.out -type f -print | wc -l)
+find .. -name *.dSYM -type d -exec rm -rf {} \; >/dev/null 2>&1
+find .. -name *.out -type f -exec rm -rf {} \; >/dev/null 2>&1
+new_dir=$(find .. -name *.dSYM -type d -print | wc -l)
+new_file=$(find .. -name *.out -type f -print | wc -l)
+
+echo "finished"
+echo "cleand $((old_dir - new_dir))/$((old_dir)) directories"
+echo "cleand $((old_file - new_file))/$((old_file)) files"
