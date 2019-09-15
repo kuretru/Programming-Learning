29cc8029e0f4eb2ab8b0ac3a7f612f2527bd07c0
呉真 <kuretru@gmail.com>
2019-09-15 16:50:14 +0800

    新增：关于unordered_map的研究

--- PAT/Advanced-Level/1041 Be Unique.cpp
+++ PAT/Advanced-Level/1041 Be Unique.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
 
 int main()
 {
     std::ios::sync_with_stdio(false);
-    std::vector<int> data;
-    std::unordered_map<int, int> map;
+    std::unordered_map<int, int> data(10001);
     int N = 0, value = 0;
     std::cin >> N;
     while (N--)
     {
         std::cin >> value;
-        if (!map.count(value))
-            data.push_back(value);
-        map[value]++;
+        data[value]++;
     }
     int result = -1;
-    for (auto iter = data.begin(); result == -1 && iter != data.end(); iter++)
-        if (map[*iter] == 1)
-            result = *iter;
+    for (auto iter = data.begin(); iter != data.end(); iter++)
+        if (iter->second == 1)
+            result = iter->first;
     if (result != -1)
         std::cout << result << std::endl;
     else
