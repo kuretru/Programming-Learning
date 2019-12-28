357e3035bd55c07cee93f9161f3d019b7d1d20cc
呉真 <kuretru@gmail.com>
2019-12-28 11:47:52 +0800

    新增：LeetCode.27 移除元素

---
+++ LeetCode/27.移除元素.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=java
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
+// @lc code=end
