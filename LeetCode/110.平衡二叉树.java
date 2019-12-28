e2942814a17fadc73f99ea05c9cfcb14c499dc32
呉真 <kuretru@gmail.com>
2019-12-28 13:40:33 +0800

    新增：LeetCode.110 平衡二叉树

---
+++ LeetCode/110.平衡二叉树.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (depth(root) == -1) {
+            return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
