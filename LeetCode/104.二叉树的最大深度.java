652a0cb2a39c1ef4c75dda26e8d90ef218e26945
呉真 <kuretru@gmail.com>
2020-01-06 16:33:25 +0800

    新增：LeetCode.104 二叉树的最大深度

---
+++ LeetCode/104.二叉树的最大深度.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=java
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
+// @lc code=end
