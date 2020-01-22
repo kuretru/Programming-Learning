e3804940243f4dadcd42dbc8ea72e9ccc2398006
呉真 <kuretru@gmail.com>
2020-01-22 16:02:51 +0800

    新增：LeetCode.121 买卖股票的最佳时机

---
+++ LeetCode/121.买卖股票的最佳时机.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > max) {
+                max = prices[i] - min;
+            }
+        }
+        return max;
+    }
+}
+// @lc code=end
