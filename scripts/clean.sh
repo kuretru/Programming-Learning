8e52cf3908231d2ee220efd45ed3dc0037e63604
呉真 <kuretru@gmail.com>
2020-01-22 16:09:52 +0800

    修复：清理脚本失效的问题

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-old_dir=$(find . -name *.dSYM -type d -print | wc -l)
-old_file=$(find . -name *.out -type f -print | wc -l)
+old_dir=`expr $(find . -name *.dSYM -type d -print | wc -l)`
+old_file=`expr $(find . -name *.out -type f -print | wc -l)`
 find . -name *.dSYM -type d -exec rm -rf {} \; >/dev/null 2>&1
 find . -name *.out -type f -exec rm -rf {} \; >/dev/null 2>&1
-new_dir=$(find . -name *.dSYM -type d -print | wc -l)
-new_file=$(find . -name *.out -type f -print | wc -l)
+new_dir=`expr $(find . -name *.dSYM -type d -print | wc -l)`
+new_file=`expr $(find . -name *.out -type f -print | wc -l)`
 
 echo "finished"
 echo "cleand $((old_dir - new_dir))/$((old_dir)) directories"
