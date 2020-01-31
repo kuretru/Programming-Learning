922074c70fc5c1e6b7050b6f233475e0679d3408
呉真 <kuretru@gmail.com>
2020-01-31 11:31:35 +0800

    新增：清理Java字节码文件

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@ old_dir=`expr $(find . -name *.dSYM -type d -print | wc -l)`
 old_file=`expr $(find . -name *.out -type f -print | wc -l)`
 find . -name *.dSYM -type d -exec rm -rf {} \; >/dev/null 2>&1
 find . -name *.out -type f -exec rm -rf {} \; >/dev/null 2>&1
+find . -name *.class -type f -exec rm -rf {} \; >/dev/null 2>&1
 new_dir=`expr $(find . -name *.dSYM -type d -print | wc -l)`
 new_file=`expr $(find . -name *.out -type f -print | wc -l)`
 
