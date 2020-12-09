52e7c40c231bbb5ebb537d0e6e710f2678e2df6d
呉真 <kuretru@gmail.com>
2020-12-09 10:39:07 +0800

    新增：LeetCode.160 相交链表

---
+++ LeetCode/160.相交链表.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA, q = headB;
+        while (p != q) {
+            p = (p == null) ? headB : p.next;
+            q = (q == null) ? headA : q.next;
+        }
+        return p;
+    }
+}
+// @lc code=end
