7ac5f07b91b6871e5f88676bb36c8fce54615afe
呉真 <kuretru@gmail.com>
2020-12-21 11:06:00 +0800

    新增：LeetCode.746 使用最小花费爬楼梯

---
+++ LeetCode/746.使用最小花费爬楼梯.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=java
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+        return dp[cost.length];
+    }
+}
+// @lc code=end
