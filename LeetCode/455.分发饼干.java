1f82fc6c77a9c7ba7d12526a473feee837317ba9
呉真 <kuretru@gmail.com>
2020-12-25 12:20:41 +0800

    新增：LeetCode.455 分发饼干

---
+++ LeetCode/455.分发饼干.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=455 lang=java
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int result = 0;
+        for (int i = 0, j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                i++;
+                result++;
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
