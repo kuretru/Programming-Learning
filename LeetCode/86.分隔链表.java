3497fbc7712763f47dbd40c9de94fdf28b534db1
呉真 <kuretru@gmail.com>
2021-01-04 20:03:52 +0800

    新增：LeetCode.86 分隔链表

---
+++ LeetCode/86.分隔链表.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=java
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null) {
+            return null;
+        }
+        ListNode smallRoot = new ListNode(0);
+        ListNode small = smallRoot;
+        ListNode largeRoot = new ListNode(0);
+        ListNode large = largeRoot;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        small.next = largeRoot.next;
+        large.next = null;
+        return smallRoot.next;
+    }
+}
+// @lc code=end
