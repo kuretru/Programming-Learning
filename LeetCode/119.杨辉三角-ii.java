69096a65306cdbf225f9ab5cd2dd19b5dc9d174b
呉真 <kuretru@gmail.com>
2021-02-12 14:25:29 +0800

    新增：LeetCode.119 杨辉三角 II

---
+++ LeetCode/119.杨辉三角-ii.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=java
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        Integer[] result = new Integer[rowIndex + 1];
+        result[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            result[i] = (int) ((long) result[i - 1] * (rowIndex - i + 1) / i);
+        }
+        return Arrays.asList(result);
+    }
+}
+// @lc code=end
