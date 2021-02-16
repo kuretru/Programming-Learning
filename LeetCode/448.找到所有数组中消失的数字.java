6b66b212fa605bfaee206db80ce29295cb4b599f
呉真 <kuretru@gmail.com>
2021-02-16 19:30:39 +0800

    新增：LeetCode.448 找到所有数组中消失的数字

---
+++ LeetCode/448.找到所有数组中消失的数字.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=448 lang=java
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        List<Integer> result = new ArrayList<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
