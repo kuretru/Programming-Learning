330753fe26624e27c56ac1f6497545e1e4c90712
呉真 <kuretru@gmail.com>
2021-02-22 22:27:39 +0800

    新增：LeetCode.766 托普利茨矩阵

---
+++ LeetCode/766.托普利茨矩阵.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=766 lang=java
+ *
+ * [766] 托普利茨矩阵
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
