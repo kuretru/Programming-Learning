c50038c4a285fbc59d9902d917539bc603702c08
呉真 <kuretru@gmail.com>
2021-04-15 19:42:23 +0800

    新增：LeetCode.213 打家劫舍 II

---
+++ LeetCode/213.打家劫舍-ii.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int x = solve(nums, 1, nums.length);
+        int y = solve(nums, 0, nums.length - 1);
+        return Math.max(x, y);
+    }
+
+    private int solve(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i < end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end - 1];
+    }
+}
+// @lc code=end
