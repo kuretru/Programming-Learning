e7b03e560a84f1a9725daa817581444d2ca2bf6b
呉真 <kuretru@gmail.com>
2021-04-27 11:23:51 +0800

    新增：LeetCode.938 二叉搜索树的范围和

---
+++ LeetCode/938.二叉搜索树的范围和.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=938 lang=java
+ *
+ * [938] 二叉搜索树的范围和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (low <= root.val && root.val <= high) {
+            sum += root.val;
+        }
+        if (root.val > low) {
+            sum += rangeSumBST(root.left, low, high);
+        }
+        if (root.val < high) {
+            sum += rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+}
+// @lc code=end
