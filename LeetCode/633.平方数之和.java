b5226a438c86e9e6c1a0506430f201aa49b5bc9a
呉真 <kuretru@gmail.com>
2021-04-28 17:44:09 +0800

    新增：LeetCode.633 平方数之和

---
+++ LeetCode/633.平方数之和.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=633 lang=java
+ *
+ * [633] 平方数之和
+ */
+
+// @lc code=start
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int)Math.sqrt(c);
+        while (left <= right) {
+            int sum = left * left + right * right;
+            if (sum == c) {
+                return true;
+            } else if (sum > c) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
