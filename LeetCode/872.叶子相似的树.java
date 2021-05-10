749ee314d21a09f14b257d2c97e3daa3a94e73ed
呉真 <kuretru@gmail.com>
2021-05-10 10:16:50 +0800

    新增：LeetCode.872 叶子相似的树

---
+++ LeetCode/872.叶子相似的树.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=872 lang=java
+ *
+ * [872] 叶子相似的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        getLeaves(root1, leaves1);
+        getLeaves(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    public void getLeaves(TreeNode root, List<Integer> leaves) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leaves.add(root.val);
+            return;
+        }
+        getLeaves(root.left, leaves);
+        getLeaves(root.right, leaves);
+    }
+}
+// @lc code=end
