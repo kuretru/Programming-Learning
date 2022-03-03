70688140affde0dcee49724ad09f2b81e378a98e
呉真 <kuretru@gmail.com>
2022-03-03 13:34:10 +0800

    新增：LeetCode.258 各位相加

---
+++ LeetCode/258.各位相加.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=java
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+class Solution {
+    public int addDigits(int num) {
+        while (num > 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+// @lc code=end
